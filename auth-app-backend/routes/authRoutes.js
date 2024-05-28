12b1fde3824084ec93003cfdfd98a78727a8677e
Sumit <akoliyasumit@hotmail.com>
2024-05-28 23:10:24 +0100

    Updating /protected to return userid and pwd

--- auth-app-backend/routes/authRoutes.js
+++ auth-app-backend/routes/authRoutes.js
@@ -2,13 +2,23 @@
 const express = require('express');
 const { register, login } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
+const User = require('../models/User');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 
-router.get('/protected', protect, (req, res) => {
-  res.json({ message: 'Welcome to the protected route', user: req.user });
+router.get('/protected', protect, async (req, res) => {
+  try {
+    const user = await User.findById(req.user);
+    res.json({
+      id: user._id,
+      username: user.username,
+      // add more user details if needed
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 module.exports = router;
